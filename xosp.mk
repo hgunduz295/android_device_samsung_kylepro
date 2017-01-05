677144d2236b84253c81ada734cb93ff18b4ef6a
sostk <poudelswastik@gmail.com>
2017-01-06 10:08:28 +1100

    Update and rename lineage.mk to xosp.mk

---
+++ xosp.mk
@@ -0,0 +1,17 @@
+# Inherit some common CM stuff.
+$(call inherit-product, vendor/xosp/config/common_mini_phone.mk)
+$(call inherit-product, vendor/xosp/config/xosp.mk)
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit device configuration
+$(call inherit-product, device/samsung/kyleprods/device_kylepro.mk)
+
+# Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := kyleprods
+PRODUCT_NAME := xosp_kylepro
+PRODUCT_BRAND := samsung
+PRODUCT_MANUFACTURER := samsung
+PRODUCT_MODEL := GT-S7580
+PRODUCT_CHARACTERISTICS := phone
