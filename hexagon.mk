cc4a9f16213a0ac7b622a04ec5f2d28c4f3e0fdd
sostk <poudelswastik@gmail.com>
2017-03-09 01:45:12 +1100

    Added support for hexagon for kylepro

--- hexagon.mk
+++ hexagon.mk
@@ -1,6 +1,5 @@
 # Inherit some common CM stuff.
-$(call inherit-product, vendor/xosp/config/common_mini_phone.mk)
-$(call inherit-product, vendor/xosp/config/xosp.mk)
+$(call inherit-product, vendor/hexagon/configs/common.mk)
 
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
@@ -10,7 +9,7 @@ $(call inherit-product, device/samsung/kylepro/device_kylepro.mk)
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := kylepro
-PRODUCT_NAME := xosp_kylepro
+PRODUCT_NAME := hexagon_kylepro
 PRODUCT_BRAND := samsung
 PRODUCT_MANUFACTURER := samsung
 PRODUCT_MODEL := GT-S7580
